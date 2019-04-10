4d837f48a612f5167d92f26c18d14d951c250742
Alexander <ilordarkey@gmail.com>
2019-04-10 14:49:27 +0300

    Updates

---
+++ precache-manifest.ab4f17cd54e1ce32b705fbabb241d10a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcbb5b439d236a873f337fc600cf260c",
+    "url": "/Typist/static/media/sound_success.dcbb5b43.mp3"
+  },
+  {
+    "revision": "7768c1d08a2ccb9be8d865eb81b94993",
+    "url": "/Typist/static/media/sound_failure.7768c1d0.mp3"
+  },
+  {
+    "revision": "404ce31e8a30b6abcd19",
+    "url": "/Typist/static/js/runtime~main.95a67731.js"
+  },
+  {
+    "revision": "acabe279fda862dd7f85",
+    "url": "/Typist/static/js/main.24e6e820.chunk.js"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/Typist/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "acabe279fda862dd7f85",
+    "url": "/Typist/static/css/main.60cb0586.chunk.css"
+  },
+  {
+    "revision": "3776985da43a078c704f8a63f0406929",
+    "url": "/Typist/index.html"
+  }
+];
